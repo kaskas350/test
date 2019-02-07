d8f24d853cd8ec236f5a889ee76e5c117c9b20d2
sergey <serge-kopytov@yandex.ru>
2019-02-07 11:53:28 +0300

    commit 2

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-//echo "Location: {$_SERVER['DOCUMENT_ROOT']}"."test/index.php";
-//header( "Location: {$_SERVER['DOCUMENT_ROOT']}test/Component/authorization/authorization.php");
-foreach ($_SERVER as $k=>$m)
-{
-    echo "[$k]=$m<br>";
-
-}
